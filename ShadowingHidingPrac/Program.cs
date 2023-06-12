512cbbced613be59ce79e0450c689be7a329e6df
Jihye Yang <s2127@e-mirim.hs.kr>
2023-06-12 13:44:46 +0900

    메서드 오버라이딩 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -14,12 +14,20 @@ namespace ShadowingHidingPrac
             {
                 Console.WriteLine("부모의 메서드");
             }
+            public virtual void Method2()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
         }
 
         class Child : Parent
         {
             public new string variable = "shadowing";
             public new void Method()
+            {
+                Console.WriteLine("자식의 메서드");
+            }
+            public override void Method2()
             {
                 Console.WriteLine("자식의 메서드");
             }
@@ -37,6 +45,9 @@ namespace ShadowingHidingPrac
 
             child.Method();
             p.Method();
+
+            child.Method2();
+            p.Method2();
         }
     }
 }
