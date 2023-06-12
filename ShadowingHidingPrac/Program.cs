6210ee7c9365456c95804719e84eda0798694182
Jihye Yang <s2127@e-mirim.hs.kr>
2023-06-12 13:38:55 +0900

    메서드 하이딩 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ShadowingHidingPrac
 {
     internal class Program
@@ -11,11 +10,19 @@ namespace ShadowingHidingPrac
         class Parent
         {
             public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
         }
 
         class Child : Parent
         {
             public new string variable = "shadowing";
+            public new void Method()
+            {
+                Console.WriteLine("자식의 메서드");
+            }
         }
 
         public static int number = 10;
@@ -27,6 +34,9 @@ namespace ShadowingHidingPrac
             Console.WriteLine(child.variable);
             Parent p = child;
             Console.WriteLine(p.variable);
+
+            child.Method();
+            p.Method();
         }
     }
 }
